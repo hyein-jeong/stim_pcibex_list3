d972469b676c7a86ed51bd07a84b7372252dc526
hyein-jeong <46813089+hyein-jeong@users.noreply.github.com>
2021-01-07 16:22:42 +0100

    Update main.js
    include instruction_prac_feedback

--- data_includes/main.js
+++ data_includes/main.js
@@ -63,6 +63,7 @@ Sequence("intro_ID",
 "preload_prac_cb",
 "instruct_2_prac_cblock",
 randomize("prac_cb"),
+"instruct_prac_cblock_fb"
 "preload_pretrain_cb",
 "instruct_3_cblock_pretrain",
 "pretrain_cb",
@@ -70,6 +71,7 @@ randomize("prac_cb"),
 "preload_prac_ncb",
 "instruct_5_prac_ncblock",
 randomize("prac_ncb"),
+"instruct_prac_ncblock_fb"
 "preload_pretrain_ncb",
 "instruct_6_ncblock_pretrain",
 "pretrain_ncb",
@@ -402,6 +404,18 @@ newTrial("instruct_2_prac_cblock",
         .wait()
 );
 
+newTrial("instruct_prac_cblock_fb",
+    defaultText
+        .print()
+    ,
+    newImage("pic_instruct", "day1_instruction_prac_cblock_ans.png")
+        .size(1280, 720)
+        .print()
+    ,
+    newKey("space", " ")
+	.log()
+        .wait()
+);
 
 newTrial("instruct_3_cblock_pretrain",
     defaultText
@@ -444,6 +458,19 @@ newTrial("instruct_5_prac_ncblock",
         .wait()
 );
 
+
+newTrial("instruct_prac_ncblock_fb",
+    defaultText
+        .print()
+    ,
+    newImage("pic_instruct", "day1_instruction_prac_ncblock_ans.png")
+        .size(1280, 720)
+        .print()
+    ,
+    newKey("space", " ")
+	.log()
+        .wait()
+);
 
 newTrial("instruct_6_ncblock_pretrain",
     defaultText
